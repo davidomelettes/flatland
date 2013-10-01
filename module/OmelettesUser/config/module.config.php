306d6db3e9a9c9068bbe9bf5671c2f2f5d2209e4
dave <david@omelett.es>
2013-10-01 16:57:12 +0100

    Creating user module from stub

---
+++ module/OmelettesUser/config/module.config.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+	'controllers' => array(
+		'invokables' => array(
+		),
+	),
+	'router' => array(
+		'routes' => array(
+		),
+	),
+	'service_manager' => array(
+	),
+	'view_helpers'	=> array(
+		'invokables'	=> array(
+		),
+	),
+	'view_manager' => array(
+		'template_map' => array(
+		),
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+	),
+);
