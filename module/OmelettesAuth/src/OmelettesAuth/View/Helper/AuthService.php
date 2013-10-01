27162554b23f5e03c217e645aee337d176473bb7
dave <david@omelett.es>
2013-10-01 11:51:15 +0100

    Adding authService view helper

---
+++ module/OmelettesAuth/src/OmelettesAuth/View/Helper/AuthService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace OmelettesAuth\View\Helper;
+
+use Zend\Authentication\AuthenticationService;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\View\Helper\AbstractHelper;
+
+class AuthService extends AbstractHelper implements ServiceLocatorAwareInterface 
+{
+	/**
+	 * @var AuthenticationService
+	 */
+	protected $authService;
+	
+	/**
+	 * @var ServiceLocatorInterface
+	 */
+	protected $serviceLocator;
+	
+	public function __invoke()
+	{
+		return $this->getAuthService();
+	}
+	
+	/**
+	 * Set the service locator.
+	 *
+	 * @param  ServiceLocatorInterface $serviceLocator
+	 */
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+	{
+		$this->serviceLocator = $serviceLocator;
+	}
+	
+	/**
+	 * Get the service locator.
+	 *
+	 * @return ServiceLocatorInterface
+	 */
+	public function getServiceLocator()
+	{
+		return $this->serviceLocator;
+	}
+	
+	/**
+	 * Returns the application service manager
+	 * 
+	 * @return ServiceLocatorInterface
+	 */
+	public function getApplicationServiceLocator()
+	{
+		// View helpers implementing ServiceLocatorAwareInterface are given Zend\View\HelperPluginManager!
+		return $this->getServiceLocator()->getServiceLocator();
+	}
+	
+	/**
+	 * Returns the authentication service used by the application
+	 * 
+	 * @return AuthenticationService
+	 */
+	public function getAuthService()
+	{
+		if (!$this->authService) {
+			$this->authService = $this->getApplicationServiceLocator()->get('AuthService');
+		}
+		
+		return $this->authService;
+	}
+	
+}
