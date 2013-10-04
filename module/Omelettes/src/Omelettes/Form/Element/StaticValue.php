c6af8fffdd4466861f50659761e820861bf2f39d
dave <david@omelett.es>
2013-10-04 11:47:04 +0100

    Allow staticValue elements to specify no html escaping

--- module/Omelettes/src/Omelettes/Form/Element/StaticValue.php
+++ module/Omelettes/src/Omelettes/Form/Element/StaticValue.php
@@ -6,7 +6,33 @@ use Zend\Form\Element;
 
 class StaticValue extends Element
 {
+	protected $escapeHtml = true;
+	
 	protected $attributes = array(
 		'type' => 'static',
 	);
+	
+	public function setOptions($options)
+	{
+		parent::setOptions($options);
+		
+		if (isset($options['escape_html'])) {
+			$this->setEscapeHtml($options['escape_html']);
+		}
+		
+		return $this;
+	}
+	
+	public function setEscapeHtml($escape)
+	{
+		$this->escapeHtml = (boolean)$escape;
+		
+		return $this;
+	}
+	
+	public function getEscapeHtml()
+	{
+		return $this->escapeHtml;
+	}
+	
 }
