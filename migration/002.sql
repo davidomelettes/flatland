7522bf8bf799341b03e45e0fccd45e415887565b
dave <david@omelett.es>
2013-10-08 11:36:36 +0100

    Adding secondary locale sql

--- migration/002.sql
+++ migration/002.sql
@@ -8,8 +8,18 @@ CREATE TABLE locales (
 COPY locales (code, name) FROM stdin;
 en_GB	English (United Kingdom)
 en_US	English (United States)
+fr_FR	French (France)
+de_DE	German (Germany)
+it_IT	Italian (Italy)
+es_ES	Spanish (Spain)
 \.
 
 ALTER TABLE users ADD COLUMN locale VARCHAR NOT NULL REFERENCES locales(code) DEFAULT 'en_GB';
+
+CREATE TABLE user_secondary_locales (
+	user_key UUID NOT NULL REFERENCES users(key),
+	locale_code VARCHAR NOT NULL REFERENCES locales(code),
+	PRIMARY KEY (user_key, locale_code)
+);
 
 COMMIT;
--- migration/migrate.sh
+++ migration/migrate.sh
@@ -2,3 +2,4 @@
 
 psql flatland < 000.sql
 psql flatland < 001.sql
+psql flatland < 002.sql
