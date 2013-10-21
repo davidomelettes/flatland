24bd55b3b5a54cc74fd306f4bc37e25634389c70
dave <david@omelett.es>
2013-10-21 12:43:42 +0100

    Removing add form from game module

--- module/FlatlandGame/src/FlatlandGame/Controller/GamesController.php
+++ module/FlatlandGame/src/FlatlandGame/Controller/GamesController.php
@@ -2,38 +2,13 @@
 
 namespace FlatlandGame\Controller;
 
-use FlatlandGame\Form;
 use Omelettes\Controller\AbstractController;
 
 class GamesController extends AbstractController
 {
-	/**
-	 * @var Form\GameForm
-	 */
-	protected $gameForm;
-	
-	public function getGameForm()
-	{
-		if (!$this->gameForm) {
-			$form = $this->getServiceLocator()->get('FormElementManager')->get('FlatlandGame\Form\GameForm');
-			$this->gameForm = $form;
-		}
-		
-		return $this->gameForm;
-	}
-	
 	public function indexAction()
 	{
 		return array();
-	}
-	
-	public function addAction()
-	{
-		$form = $this->getGameForm();
-		
-		return array(
-			'form' => $form,
-		);
 	}
 	
 	public function viewAction()
