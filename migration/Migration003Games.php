64995e413339c31c17392b25fce90129afc8d496
dave <david@omelett.es>
2013-11-04 12:26:50 +0000

    Simplifies early schema design for games

--- migration/Migration003Games.php
+++ migration/Migration003Games.php
@@ -12,32 +12,15 @@ class Migration003Games extends AbstractMigration
 		
 		$this->tableCreate('designers', $this->getQuantumTableColumns());
 		
-		$this->tableCreate('games', $this->getQuantumTableColumns());
-		
-		$this->tableCreate('game_locale_details', array(
-			'game_key'				=> 'UUID NOT NULL REFERENCES games(key)',
-			'language_code'			=> 'CHAR(2) NOT NULL REFERENCES locale_languages(code)',
-			'name'					=> 'VARCHAR',
+		$this->tableCreate('games', array_merge($this->getQuantumTableColumns(), array(
 			'description'			=> 'TEXT',
-		), array('game_key', 'language_code'));
+			'publisher_key'			=> "UUID REFERENCES publishers(key)", 
+		)));
 		
 		$this->tableCreate('game_designers', array(
 			'game_key'				=> 'UUID NOT NULL REFERENCES games(key)',
 			'designer_key'			=> 'UUID NOT NULL REFERENCES designers(key)',
 		), array('game_key', 'designer_key'));
-		
-		$this->tableCreate('game_products', array_merge($this->getQuantumTableColumns(), array(
-			'game_key'				=> 'UUID NOT NULL REFERENCES games(key)',
-			'publisher_key'			=> 'UUID REFERENCES publishers(key)',
-			'published'				=> 'DATE',
-			'published_precision'	=> "INT NOT NULL DEFAULT '1'",
-			'edition'				=> "INT NOT NULL DEFAULT '1'",
-		)));
-		
-		$this->tableCreate('game_product_languages', array(
-			'product_key'			=> 'UUID NOT NULL REFERENCES game_products(key)',
-			'language_code'			=> 'CHAR(2) NOT NULL REFERENCES locale_languages(code)',
-		), array('product_key', 'language_code'));
 		
 		$this->insertFixture('migration/fixtures/003_games.xml');
 		
