28565640c244656417f829e5378c4a7b2dc63edc
dave <david@omelett.es>
2013-11-21 12:45:58 +0000

    Removing duplicate migration

--- migration/Migration004Groups.php
+++ migration/Migration004Groups.php
@@ -9,18 +9,12 @@ class Migration004Groups extends AbstractMigration
 	public function migrate()
 	{
 		$this->tableCreate('groups', array_merge($this->getQuantumTableColumns(), array(
-			'private'				=> "BOOLEAN NOT NULL DEFAULT 'true'",
 		)));
 		
-		$this->tableCreate('group_users', array(
-			'group_key'				=> "UUID NOT NULL REFERENCES groups(key)",
-			'user_key'				=> "UUID NOT NULL REFERENCES users(key)",
-		), array('group_key', 'user_key'));
-		
-		$this->tableCreate('user_libraries', array(
-			'user_key'				=> "UUID NOT NULL REFERENCES users(key)",
-			'game_key'				=> "UUID NOT NULL REFERENCES games(key)",
-		));
+		$this->tableCreate('group_users', array_merge(array(
+			'group_key' => 'UUID NOT NULL REFERENCES lists(key)',
+			'user_key' => 'UUID NOT NULL REFERENCES games(key)',
+		)), array('group_key', 'user_key'));
 		
 		return true;
 	}
